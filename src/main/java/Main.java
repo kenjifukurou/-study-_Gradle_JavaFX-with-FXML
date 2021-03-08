f721ff57ae33c76e973ffc4d9ca5e9e8beaae0b9
Kenji Fukurou <59118789+kenjifukurou@users.noreply.github.com>
2021-03-08 16:31:02 +0800

    Added BorderPane
    Added example of "Responsive UI" fxml using BorderPane, ImageView inserted to Button to create Button's Icon

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("new_sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample_responsive.fxml"));
 
         Scene scene = new Scene(root, 600, 400);
         scene.getStylesheets().add("styles.css");
